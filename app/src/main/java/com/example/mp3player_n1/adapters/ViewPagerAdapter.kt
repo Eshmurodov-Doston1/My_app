eebca032bae7ac42227f17aa80b39e65cec91d47
Eshmurodov-Doston1 <eshmurodovdoston630@gmail.com>
2021-04-16 20:55:01 +0500

    My_app

--- app/src/main/java/com/example/mp3player_n1/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/mp3player_n1/adapters/ViewPagerAdapter.kt
@@ -2,17 +2,34 @@ package com.example.mp3player_n1.adapters
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.mp3player_n1.fragments.Homeragment
 import com.example.mp3player_n1.fragments.SettingsFragment
 import com.example.mp3player_n1.models.Category
 
-class ViewPagerAdapter(var list: List<Category>,fragmentActivity: FragmentActivity):FragmentStateAdapter(fragmentActivity) {
-    override fun getItemCount(): Int {
+class ViewPagerAdapter(var list: List<Category>,fragmentManager: FragmentManager):FragmentStatePagerAdapter(fragmentManager,
+BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    override fun getCount(): Int {
         return list.size
     }
 
-    override fun createFragment(position: Int): Fragment {
-        return SettingsFragment.newInstance(list[position].name!!, list[position].position!!)
+    override fun getItem(position: Int): Fragment {
+        when(position){
+            0->{
+                return   Homeragment()
+            }
+            1->{
+                return   SettingsFragment.newInstance(list[position].name!!, list[position].position!!)
+            }
+            2->{
+                return Homeragment()
+            }
+            else ->{
+                return Homeragment()
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/mp3player_n1/fragments/MenuFragment.kt
+++ app/src/main/java/com/example/mp3player_n1/fragments/MenuFragment.kt
@@ -1,10 +1,20 @@
 package com.example.mp3player_n1.fragments
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.drawable.Icon
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.viewpager.widget.ViewPager
 import com.example.mp3player_n1.R
+import com.example.mp3player_n1.adapters.ViewPagerAdapter
 import com.example.mp3player_n1.databinding.FragmentMenuBinding
+import com.example.mp3player_n1.models.Category
+import nl.joery.animatedbottombar.AnimatedBottomBar
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,14 +40,92 @@ class MenuFragment : Fragment() {
     }
     lateinit var fragmentMenuBinding:FragmentMenuBinding
     lateinit var root:View
+    lateinit var categoryList:ArrayList<Category>
+    lateinit var viewPagerAdapter: ViewPagerAdapter
+    private  val TAG = "MenuFragment"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         fragmentMenuBinding = FragmentMenuBinding.inflate(inflater,container,false)
         root = fragmentMenuBinding.root
+        loadCategory()
+        setUpView()
+        fragmentMenuBinding.viewPager.adapter =viewPagerAdapter
+        fragmentMenuBinding.bottomNavigation.onTabSelected={
+           when(it.title.toLowerCase()){
+               "home".toLowerCase()->{
+                   fragmentMenuBinding.viewPager.currentItem = 0
+                   fragmentMenuBinding.bottomNavigation.tabColorSelected = Color.parseColor("#FF5722")
+                   fragmentMenuBinding.bottomNavigation.indicatorColor = Color.parseColor("#FF5722")
+                   fragmentMenuBinding.bottomNavigation.rippleColor = Color.parseColor("#FF5722")
+               }
+               "settings".toLowerCase()->{
+                   fragmentMenuBinding.viewPager.currentItem = 1
+                   fragmentMenuBinding.bottomNavigation.tabColorSelected = Color.parseColor("#0707B1")
+                   fragmentMenuBinding.bottomNavigation.rippleColor = Color.parseColor("#0707B1")
+                   fragmentMenuBinding.bottomNavigation.indicatorColor= Color.BLUE
 
+               }
+               "user".toLowerCase()->{
+                   fragmentMenuBinding.viewPager.currentItem = 2
+                   fragmentMenuBinding.bottomNavigation.tabColorSelected = Color.parseColor("#930234")
+                   fragmentMenuBinding.bottomNavigation.indicatorColor = Color.parseColor("#930234")
+                   fragmentMenuBinding.bottomNavigation.rippleColor = Color.parseColor("#930234")
+               }
+           }
+        }
+
+        var click = object: OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                if(fragmentMenuBinding.viewPager.currentItem==0){
+                   activity!!.finish()
+                }else{
+                    fragmentMenuBinding.viewPager.currentItem=0
+                }
+            }
+            }
+               requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,click)
         return root
+    }
+
+    @SuppressLint("UseRequireInsteadOfGet")
+    private fun setUpView() {
+        viewPagerAdapter = ViewPagerAdapter(categoryList, fragmentManager!!)
+        fragmentMenuBinding.viewPager.adapter =viewPagerAdapter
+        fragmentMenuBinding.viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                when (position) {
+                    0 -> {
+                        fragmentMenuBinding.bottomNavigation.selectTabById(R.id.home,true)
+                    }
+                    1 -> {
+                        fragmentMenuBinding.bottomNavigation.selectTabById(R.id.settings,true)
+                        Log.d(TAG, "onPageSelected: ${fragmentMenuBinding.bottomNavigation.selectedIndex}")
+                    }
+                    2 -> {
+                        fragmentMenuBinding.bottomNavigation.selectTabById(R.id.user ,true)
+                        Log.d(TAG, "onPageSelected: ${fragmentMenuBinding.bottomNavigation.selectedIndex}")
+                    }
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+        })
+
+    }
+
+    private fun loadCategory() {
+    categoryList = ArrayList()
+    categoryList.add(Category("Home",R.drawable.ic_home,0))
+    categoryList.add(Category("Settings",R.drawable.ic_settings,1))
+    categoryList.add(Category("User",R.drawable.ic_user__1_,2))
     }
 
 
